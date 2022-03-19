76d1736918cd7cb1cb545c0d80074560ce9e83b1
Kanishk Gupta <kanishk333gupta@gmail.com>
2022-03-19 23:02:20 +0530

    updated gitignore
    Signed-off-by: Kanishk Gupta <kanishk333gupta@gmail.com>

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongoURI:'mongodb+srv://kanishk:Dj9Een8jGk2kUF@@cluster111.bbdgi.mongodb.net/webdb2?retryWrites=true&w=majority'
-}
-
-// mongodb+srv://kanishk:<Dj9Een8jGk2kUF@>@cluster111.bbdgi.mongodb.net/webdb2?retryWrites=true&w=majority
